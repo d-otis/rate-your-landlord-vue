0ab7d634382e231807724630a3bca6ace4233b52
d-otis <dan@dan-foley.com>
2021-01-19 13:49:35 -0500

    import and register Landlord components inside of Landlords component

--- src/components/landlords/Landlords.vue
+++ src/components/landlords/Landlords.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-  Landlords Index    
+  <h1>Landlords Index</h1>
+  <div v-for="landlord in landlords" v-bind:key="landlord.id">
+    <Landlord v-bind:landlord="landlord" />  
+  </div>  
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Landlord from './Landlord'
 
 export default {
   name: "Landlords",
+  components: {
+    Landlord
+  },
   data() {
     return {
       landlords: []

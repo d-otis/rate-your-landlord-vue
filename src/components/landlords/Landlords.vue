06a5be5b01497484cd8a8531e120fa3e14c5764c
d-otis <dan@dan-foley.com>
2021-01-19 12:07:23 -0500

    add Landlords component to components

---
+++ src/components/landlords/Landlords.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  Landlords Index    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Landlords",
+  data() {
+    return {
+      landlords: []
+    }
+  },
+  created() {
+    this.landlords = ["a landlord"]
+  }
+}
+</script>

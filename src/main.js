b803b14f234ecf17fa9fcd7b1edc7f378bd15f88
d-otis <dan@dan-foley.com>
2021-01-19 17:55:18 -0500

    import and register danStore

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import danStore from './danStore'
 
 Vue.config.productionTip = false
 
 new Vue({
+  data: danStore,
   router,
   render: h => h(App)
 }).$mount('#app')

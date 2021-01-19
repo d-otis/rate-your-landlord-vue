e95a99fd286f737841782ba0cb339363f6c63bd6
d-otis <dan@dan-foley.com>
2021-01-19 13:48:33 -0500

    add Landlord component to files

---
+++ src/components/landlords/Landlord.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="landlord">
+    <h2>{{ landlord.attributes.name }}</h2>
+    <ul>
+      <li>Number of Properties: {{ landlord.relationships.properties.data.length }}</li>
+      <li>Rating: {{ rating }}</li>
+    </ul>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Landlord",
+  props: ["landlord"],
+  computed: {
+    rating() {
+      return this.landlord.attributes.rating.toFixed(2)
+    }
+  }
+}
+</script>

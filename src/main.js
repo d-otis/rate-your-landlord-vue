973ae85c63383bf0704e092f5af0addf70fee98e
d-otis <dan@dan-foley.com>
2021-01-20 15:39:01 -0500

    remove custom state mgmt

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import danStore from './danStore'
 import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  // data: danStore,
   router,
   store,
   render: h => h(App)

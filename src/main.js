a75fa408a5e0c2af90108ce5bf058d251874e0b5
d-otis <dan@dan-foley.com>
2021-01-20 14:40:18 -0500

    replace custom store with vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import danStore from './danStore'
+// import danStore from './danStore'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  data: danStore,
+  // data: danStore,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')

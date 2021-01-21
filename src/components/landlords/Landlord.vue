813ee91264f862e4c441f1b0d26d287192dec21c
d-otis <dan@dan-foley.com>
2021-01-21 17:07:52 -0500

    refactor Landlord show to incorporate Vuex getters

--- src/components/landlords/Landlord.vue
+++ src/components/landlords/Landlord.vue
@@ -2,26 +2,21 @@
   <div class="landlord">
     <button @click="$router.go(-1)">Go Back</button>
     <h2>Landlord Show</h2>
-    <p>{{ landlord.attributes.name }}</p>
+    <p>{{ landlord.name }}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: "Landlord",
   props: ["landlordId"],
   computed: {
+    ...mapGetters(['getLandlordById']),
     landlord() {
-      return this.$root.$data.state.landlords.find(ll => ll.id === this.landlordId )
-    },
-    numProperties() {
-      return this.landlord.relationships.properties.data.length
-    },
-    name() {
-      return this.landlord.attributes.name
-    },
-    propertiesArray() {
-      return this.landlord.relationships.properties.data
+      const id = this.$route.params.landlordId
+      return this.getLandlordById(id)
     }
   }
 }

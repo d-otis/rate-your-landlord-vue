13a86305116c9c57dc22424d2ba88937709db31f
d-otis <dan@dan-foley.com>
2021-01-25 12:03:23 -0500

    remove Number.toFixed() as it is being handled via utility function in landlords store module

--- src/components/landlords/Landlords.vue
+++ src/components/landlords/Landlords.vue
@@ -15,7 +15,7 @@
       </thead>
       <tr v-for="landlord in allLandlords" :key="landlord.id">
         <td>{{ landlord.name }}</td>
-        <td>{{ landlord.rating.toFixed(2) }}</td>
+        <td>{{ landlord.rating }}</td>
         <td>{{ landlord.properties.length }}</td>
         <td><router-link v-bind:to="{ name: 'landlord', params: {landlordId: landlord.id} }">view</router-link></td>
       </tr>

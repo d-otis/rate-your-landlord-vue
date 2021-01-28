f4caf623825196e5edfe455ff7ce33d829833f66
d-otis <dan@dan-foley.com>
2021-01-28 13:11:56 -0500

    add normalizing function for landlord instances

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,14 @@
 export function formatRating(rating) {
   return rating ? Number(rating).toPrecision(3) : "n/a"
 }
+
+export function normalizeLandlord(landlord) {
+  return {
+    id: landlord.id,
+    name: landlord.attributes.name,
+    rating: formatRating(landlord.attributes.rating),
+    image: landlord.attributes.image_url,
+    properties: landlord.relationships.properties.data.map(property => property.id),
+    reviews: landlord.relationships.reviews.data.map(review => review)
+  }
+}

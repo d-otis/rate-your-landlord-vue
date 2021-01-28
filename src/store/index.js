01d353858370778d1f2065a1351c36f47b3352f2
d-otis <dan@dan-foley.com>
2021-01-28 14:10:07 -0500

    import and use properties store module in store index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import landlords from './modules/landlords'
+import properties from './modules/properties'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    landlords
+    landlords,
+    properties
   }
 })

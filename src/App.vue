179de1bf8a00b500b480cf6eaa0e854da826baea
d-otis <dan@dan-foley.com>
2021-01-28 13:51:46 -0500

    add router-link for Properties component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/landlords">Landlords</router-link>
+      <router-link to="/landlords">Landlords</router-link> | 
+      <router-link to="/properties">Properties</router-link>
     </div>
     <router-view/>
   </div>

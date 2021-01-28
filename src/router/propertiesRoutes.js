33c3ebd49765f6aa6059a7fbbf67fcb05d6839e0
d-otis <dan@dan-foley.com>
2021-01-28 18:55:52 -0500

    add Property show route

--- src/router/propertiesRoutes.js
+++ src/router/propertiesRoutes.js
@@ -1,9 +1,16 @@
 import Properties from '../components/properties/Properties'
+import Property from '../components/properties/Property'
 
 export default [
   {
     path: "/properties",
     name: "Properties",
     component: Properties
+  },
+  {
+    path: "/properties/:propertyId",
+    name: "property",
+    component: Property,
+    props: route => ({ propertyId: route.params.propertyId })
   }
 ]

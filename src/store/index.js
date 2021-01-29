65aea4931b0aafaedf71f4ca3a7e42803bcc8f04
d-otis <dan@dan-foley.com>
2021-01-28 19:06:38 -0500

    add review store module to store index

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import landlords from './modules/landlords'
 import properties from './modules/properties'
+import reviews from './modules/reviews'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     landlords,
-    properties
+    properties,
+    reviews
   }
 })

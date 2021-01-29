765289f6f7bccb6e4afbae94fcf4cb11cd47d116
d-otis <dan@dan-foley.com>
2021-01-28 20:19:24 -0500

    stub out boiler for Review component

---
+++ src/components/reviews/Review.vue
@@ -0,0 +1,16 @@
+<template>
+  <p> <span class="review-rating">{{ review.rating }}</span> - {{ review.content }}</p>
+</template>
+
+<script>
+export default {
+  name: "Review",
+  props: ["review"]
+}
+</script>
+
+<style scoped>
+  .review-rating {
+    font-weight: bold;
+  }
+</style>

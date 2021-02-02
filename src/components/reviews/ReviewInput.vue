4486f7768be1b78cafa02b3f145cbfefdbabdf7b
d-otis <dan@dan-foley.com>
2021-02-02 10:19:50 -0500

    import and mapActions() into ReviewInput and add to component methods

--- src/components/reviews/ReviewInput.vue
+++ src/components/reviews/ReviewInput.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: "ReviewInput",
   props: ["propertyId"],
@@ -24,7 +26,8 @@ export default {
     handleSubmit(e) {
       e.preventDefault();
       console.log('submitted!')
-    }
+    },
+    ...mapActions(["createReview"])
   }
 }
 </script>

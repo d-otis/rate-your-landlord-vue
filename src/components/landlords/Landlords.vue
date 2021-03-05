f39b9ffd03f2456b6b7a8312a91fb1b94e6b6263
d-otis <dan@dan-foley.com>
2021-03-04 19:00:36 -0500

    italicize Loading text

--- src/components/landlords/Landlords.vue
+++ src/components/landlords/Landlords.vue
@@ -3,7 +3,7 @@
     <LandlordInput v-show="addLandlordShow" v-on:toggle-input-vis="toggle" />
      <button @click="toggle" v-show="!addLandlordShow">Add a Landlord</button>
      <button @click="toggle" v-show="addLandlordShow">Cancel</button>
-    <h1> <span v-show="!allLandlords.length">Loading: </span> Landlords Index</h1> 
+    <h1> <span v-show="!allLandlords.length">Loading: </span> Landlords Index</h1>
     <table>
       <thead>
         <tr>
@@ -63,5 +63,8 @@ export default {
     display: flex;
     flex-direction: column;
     /* align-items: center; */
+  }
+  span {
+    font-style: italic;
   }
 </style>

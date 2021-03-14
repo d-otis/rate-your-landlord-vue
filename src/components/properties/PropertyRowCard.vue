43f12182adcec6877785978f88f3756c7593398c
d-otis <dan@dan-foley.com>
2021-03-14 11:41:33 -0400

    restructure PropertyRowCard component elements

--- src/components/properties/PropertyRowCard.vue
+++ src/components/properties/PropertyRowCard.vue
@@ -2,19 +2,20 @@
   <b-card no-body class="overflow-hidden" style="max-width: 540px;">
     <b-card-header header-tag="h3">{{ property.address }}</b-card-header>
     <b-card-body>
-    <b-row no-gutters>
-      <b-col sm="4">
-        <b-card-img :src="property.image" alt="Image" class="rounded-0"></b-card-img>
-      </b-col>
-      <b-col md="8">
-        <router-link :to="{ name: 'landlord', params: { landlordId: property.landlordId } }"><b-card-title title-tag="h6">{{ property.landlordId }}</b-card-title> </router-link>
-        
-          <b-card-text text-tag="h6">
-            This is a wider card with supporting text as a natural lead-in to additional content.
-            This content is a little bit longer.
-          </b-card-text>
-      </b-col>
-    </b-row>
+      <b-container>
+        <b-row no-gutters>
+          <b-col sm="4">
+            <b-card-img :src="property.image" alt="Image" class="rounded-0"></b-card-img>
+          </b-col>
+          <b-col md="8">
+            <router-link :to="{ name: 'landlord', params: { landlordId: property.landlordId } }"><b-card-title title-tag="h6">{{ property.landlordId }}</b-card-title> </router-link>
+              <b-card-text text-tag="h6">Landlord Rating: XX</b-card-text>
+              <b-card-text text-tag="h6">Property Rating: {{ property.rating }}</b-card-text>
+              <b-card-text >This property has {{ property.reviews.length }} reviews.</b-card-text>
+              <b-button>Leave a Review || Read Reviews</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
     </b-card-body>
   </b-card>
 </template>

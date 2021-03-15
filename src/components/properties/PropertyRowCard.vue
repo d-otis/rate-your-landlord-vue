951b7d619f65832e2d2274cecc8bd75200db43e9
d-otis <dan@dan-foley.com>
2021-03-15 12:15:37 -0400

    correct component props for proper layout

--- src/components/properties/PropertyRowCard.vue
+++ src/components/properties/PropertyRowCard.vue
@@ -1,14 +1,14 @@
 <template>
-  <b-card no-body class="overflow-hidden">
+  <b-card no-body class="my-4">
     <b-card-header header-tag="h3">{{ property.address }}</b-card-header>
     <b-card-body>
       <b-container>
-        <b-row no-gutters>
+        <b-row>
           <b-col sm="4">
             <b-card-img :src="property.image" alt="Image" class="rounded-0"></b-card-img>
           </b-col>
-          <b-col md="8">
-            <router-link :to="{ name: 'landlord', params: { landlordId: property.landlordId } }"><b-card-title title-tag="h6">{{ property.landlordId }}</b-card-title> </router-link>
+          <b-col sm="8">
+            <router-link :to="{ name: 'landlord', params: { landlordId: property.landlordId } }"><b-card-title title-tag="h5">{{ property.landlordId }}</b-card-title> </router-link>
               <b-card-text text-tag="h6">Landlord Rating: XX</b-card-text>
               <b-card-text text-tag="h6">Property Rating: {{ property.rating }}</b-card-text>
               <b-card-text >This property has {{ property.reviews.length }} reviews.</b-card-text>

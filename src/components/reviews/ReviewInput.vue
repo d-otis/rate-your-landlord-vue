73e8fda35442e6acc8a93d3aac9c9f5673334c60
d-otis <dan@dan-foley.com>
2021-03-22 19:38:47 -0400

    wrap ReviewInput form in Bootstrap components for layout control

--- src/components/reviews/ReviewInput.vue
+++ src/components/reviews/ReviewInput.vue
@@ -1,14 +1,20 @@
 <template>
-  <form @submit="handleSubmit">
-    <h2>Leave a Review Here:</h2>
-    <textarea name="" id="" cols="30" rows="10" v-model="content" placeholder="type your review here!"></textarea>
-    <br>
-    <input type="range" name="" id="" min="1" max="5" step="1" v-model="rating"> Rating: {{ rating }}
-    <br>
-    <input type="submit" value="Save">
-    <br>
-   <button type="button" @click="$emit('toggle-review-input')">Cancel</button>
-  </form>
+  <b-container>
+    <b-row>
+      <b-col sm="12" class="mt-4">
+        <form @submit="handleSubmit">
+          <h2>Leave a Review Here:</h2>
+          <textarea name="" id="" cols="30" rows="10" v-model="content" placeholder="type your review here!"></textarea>
+          <br>
+          <input type="range" name="" id="" min="1" max="5" step="1" v-model="rating"> Rating: {{ rating }}
+          <br>
+          <input type="submit" value="Save">
+          <br>
+         <button type="button" @click="$emit('toggle-review-input')">Cancel</button>
+        </form>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>

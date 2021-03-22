b247a36f0f32ab5f2922c9fedc60319c59135412
d-otis <dan@dan-foley.com>
2021-03-22 19:28:13 -0400

    refactor Review component with Bootstrap components

--- src/components/reviews/Review.vue
+++ src/components/reviews/Review.vue
@@ -1,5 +1,22 @@
 <template>
-  <p> <span class="review-rating">{{ review.rating }}</span> - {{ review.content }}</p>
+  <b-row class="bg-light rounded-lg py-3 border mb-3">
+    <b-col>
+      <b-container class="inner-review-container">
+        <b-row>
+          <b-col sm="2" class="review-rating text-center my-3">
+            <h1>
+              <span class="bg-secondary text-white p-2 rounded-lg">
+                {{ review.rating }}
+              </span>
+            </h1>
+          </b-col>
+          <b-col sm="10" class="review-content">
+            <p>{{ review.content }}</p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
